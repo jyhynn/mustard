598962469484f8139e0f7afe5e98bbf17aeeeee0
jyhynn <letsjamm@naver.com>
2019-04-28 19:38:05 +0900

    페이징 기능 추가

---
+++ mustard/src/main/java/com/spring/domain/Criteria.java
@@ -0,0 +1,25 @@
+package com.spring.domain;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Criteria {
+	private int pageNum, amount;
+	private String type, keyword; // 검색
+
+	public Criteria() {
+		this(1, 10); // page1 띄우고 10개씩 보여줄것
+	}
+
+	public Criteria(int pageNum, int amount) {
+		super();
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+
+	public String[] getTypeArr() {
+		return type == null ? new String[] {} : type.split("");
+	}
+}
